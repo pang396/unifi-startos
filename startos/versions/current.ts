1d5a428c386fe1f6eec2750658692483012c3af9
Your Name <you@example.com>
2026-06-19 20:23:12 +0000

    Fix package version to match jacobalberty/unifi:v10.0.162

--- startos/versions/current.ts
+++ startos/versions/current.ts
@@ -1,8 +1,8 @@
 import { IMPOSSIBLE, VersionInfo } from '@start9labs/start-sdk'
 export const current = VersionInfo.of({
-  version: '10.4.57:1',
+  version: '10.0.162:1',
   releaseNotes: {
-    en_US: 'Initial release.',
+    en_US: 'Initial release. Wraps jacobalberty/unifi:v10.0.162.',
   },
   migrations: {
     up: async ({ effects }) => {},
